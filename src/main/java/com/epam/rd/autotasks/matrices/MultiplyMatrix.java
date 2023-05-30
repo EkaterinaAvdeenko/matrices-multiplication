4ccaa696473faa4e87752d56d7f071e304786330
Avdeenko Ekaterina <avdeenqoeka@gmail.com>
2023-05-30 22:40:25 +0200

    multiply_matrices_AvdeenkoEkaterina

--- src/main/java/com/epam/rd/autotasks/matrices/MultiplyMatrix.java
+++ src/main/java/com/epam/rd/autotasks/matrices/MultiplyMatrix.java
@@ -1,30 +1,53 @@
 package com.epam.rd.autotasks.matrices;
 import java.util.Arrays;
-
 public class MultiplyMatrix {
     public static int[][] multiply(int[][] matrix1, int[][] matrix2) {
+        int R1 = matrix1.length;
+        int R2 = matrix2.length;
+        int C2 = matrix2[0].length;
+        int C1 = matrix1[0].length;
+       int[][] rslt = new int[R1][C2];
 
-        // Put your code here
+       System.out.println();
+        int i, j, k;
+        for (i = 0; i < R1; i++) {
+            for (j = 0; j < C2; j++) {
+                    rslt[i][j] = 0;
+                    for (k=0; k<R2; k++)
+                   rslt[i][j] += matrix1[i][k] * matrix2[k][j];
+              System.out.print(rslt[i][j] + " ");
+            }
+            System.out.println("");}
 
-        return null;
+        return rslt;
     }
 
-    public static void main(String[] args) {
+        public static void main (String[]args){
 
-        System.out.println("Test your code here!\n");
+                System.out.println("Test your code here!\n");
 
-        // Get a result of your code
+            int[][] matrix1 = {
+                    {0, 12345},
+                    {4509, 0},
+                    {3, 567}};
 
-        int[][] a = {
-                {0, 12345},
-                {4509, 0},
-                {3, 567} };
+           int[][] matrix2 = {
+                   {653, 0, 25353},
+                    {0, 61, 6}};
 
-        int[][] b = {
-                {653, 0, 25353},
-                {0, 61, 6} };
+                int[][] rslt = multiply(matrix1, matrix2);
+                System.out.println(Arrays.deepToString(rslt).replace("],", "]\n"));
 
-        int[][] result = multiply(a, b);
-        System.out.println(Arrays.deepToString(result).replace("],", "]\n"));
-    }
-}
+            }}
+
+
+
+
+
+
+
+
+
+
+
+
